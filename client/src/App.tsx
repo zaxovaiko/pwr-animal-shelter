1ec0a64956682eb126088e095afb4d01e8e501fb
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2021-10-10 14:38:40 +0200

    Added AuthContext

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { AuthContext } from "./contexts/AuthContext";
+
 function App() {
+  const { auth, setAuth } = useContext(AuthContext);
+
   return <></>;
 }
 
