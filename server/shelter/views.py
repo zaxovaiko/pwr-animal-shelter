a52d5055e88c01e48599f48690317e1810fbacb6
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2021-10-18 10:43:57 +0200

    Added authorization with jwt token

---
+++ server/shelter/views.py
@@ -0,0 +1,16 @@
+from .models import User
+from rest_framework import viewsets, generics, permissions
+from shelter.serializers import UserSerializer
+from .serializers import RegisterSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = ()
+    serializer_class = RegisterSerializer
