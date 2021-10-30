768754cb1975125db4ea858228b6af419e6a8612
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2021-10-30 23:47:23 +0200

    Added UserManager

--- server/users/models.py
+++ server/users/models.py
@@ -1,5 +1,36 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -11,9 +42,10 @@ class User(AbstractUser):
     pesel = models.CharField("PESEL", max_length=11, unique=True)
     phone = models.CharField("Telefon", max_length=20)
 
+    objects = UserManager()
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'pesel', 'password']
 
     def __str__(self):
         return self.email + ' ' + self.pesel
-        
