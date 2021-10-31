31b5a8e0574f20d2c6446a4c493d512a94377851
Olga-V-K <katsko.olga@mail.ru>
2021-10-31 23:04:02 +0100

    feature/48

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Header from "./components/layout/Header/Header";
 import { AuthContext } from "./contexts/AuthContext";
 import Home from "./pages/Home";
 import Contact from "./pages/contact/Contact";
+import Charity from "./pages/charity/Charity";
 
 function App() {
   const { auth, setAuth } = useContext(AuthContext);
@@ -15,6 +16,7 @@ function App() {
       <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/contact" component={Contact} />
+          <Route exact path="/charity" component={Charity} />
       </Switch>
       <Footer />
     </>
