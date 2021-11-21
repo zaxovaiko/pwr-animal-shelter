7d09d9003e0b5450e4cc02ed84b437a2fc3a0097
Olga-V-K <katsko.olga@mail.ru>
2021-11-21 17:01:35 +0100

    [SZSZ-93] menu based on permissions

--- client/src/components/layout/Menu/Menu.tsx
+++ client/src/components/layout/Menu/Menu.tsx
@@ -1,15 +1,23 @@
-import { useState } from "react";
+import {useContext, useState} from "react";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import { fetchAnimalTypes } from "../../../api/animals";
 import styles from "./Menu.module.css";
 import SubMenu, { MenuLink } from "./SubMenu";
+import {useParams} from "react-router";
+import {AuthContext} from "../../../contexts/AuthContext";
 
 export default function Menu({ isOpened }: { isOpened: boolean }) {
   const [current, setCurrent] = useState<number | undefined>(undefined);
   const { isError, isLoading, data } = useQuery("getAnimalTypes", () =>
     fetchAnimalTypes()
   );
+
+    const { id } = useParams<{ id: string }>();
+    const { auth } = useContext(AuthContext);
+    const { isError: isError2, isLoading: isLoading2, data: User } = useQuery("fetchProfileData", () =>
+        fetch(id)
+    );
 
   if (isError) {
     return <></>;
@@ -24,12 +32,40 @@ export default function Menu({ isOpened }: { isOpened: boolean }) {
     text: value,
   }));
 
-  const links: MenuLink[] & { submenu?: MenuLink[] } = [
-    { path: "/adoption", text: "Adopcja", submenu },
-    { path: "/contact", text: "Kontakt" },
-    { path: "/charity", text: "Jak pomóc?" },
-    { path: "/reserved-animals", text: "Zarezerwowane" },
-  ];
+  function permissionCheck() {
+    let links : MenuLink[] & { submenu?: MenuLink[] } = [];
+
+    if (auth.user) {
+      if (auth.user.is_staff) {
+        links = [
+          { path: "/adoption", text: "Adopcja", submenu },
+          { path: "/contact", text: "Kontakt" },
+          { path: "/charity", text: "Jak pomóc?" },
+          { path: "/forms", text: "Formularzy" },
+          { path: "/", text: "Strona główna"},
+        ]
+      }else {
+        links = [
+          { path: "/adoption", text: "Adopcja", submenu },
+          { path: "/contact", text: "Kontakt" },
+          { path: "/charity", text: "Jak pomóc?" },
+          { path: "/reserved-animals", text: "Moje rezerwacje" },
+          { path: "/", text: "Strona główna"},
+        ]
+        return links
+      }
+    } else {
+      links = [
+        { path: "/adoption", text: "Adopcja", submenu },
+        { path: "/contact", text: "Kontakt" },
+        { path: "/charity", text: "Jak pomóc?" },
+        { path: "/", text: "Strona główna"},
+      ]
+    }
+    return links
+  }
+
+  const links: MenuLink[] & { submenu?: MenuLink[] } = permissionCheck();
 
   return (
     <ul
